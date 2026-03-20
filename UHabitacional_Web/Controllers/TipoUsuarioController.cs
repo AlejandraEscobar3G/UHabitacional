059b35d3c38a6324f234d55df60d998d2b06d7ef
Alejandra Escobar <ale.fesico@gmail.com>
2026-03-20 13:45:06 -0600

    Se agregan vistas y logica CRUD para tipos de usuario

---
+++ UHabitacional_Web/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UHabitacional_Web.Models;
+
+namespace UHabitacional_Web.Controllers
+{
+    public class TipoUsuarioController : Controller
+    {
+        private readonly UHabitacionalContext _context;
+        public TipoUsuarioController(UHabitacionalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TipoUsuarioController
+        public async Task<IActionResult> Index()
+        {
+            List<TipoUsuario> tipoUsuarios = await _context.TiposUsuario.ToListAsync();
+
+            return View(tipoUsuarios);
+        }
+
+        // GET: TipoUsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TipoUsuarioController/Create
+        public ActionResult Create()
+        {
+            return PartialView("_Create", new TipoUsuario());
+        }
+
+        // POST: TipoUsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TipoUsuario tipoUsuario)
+        {
+            try
+            {
+                tipoUsuario.Estatus = EstatusTipoUsuario.Activo;
+                tipoUsuario.CreatedAt = DateTime.Now;
+                tipoUsuario.CreatedBy = 0;
+
+                _context.TiposUsuario.Add(tipoUsuario);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: TipoUsuarioController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            TipoUsuario? tipoUsuario = await _context.TiposUsuario
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            return PartialView("_Edit", tipoUsuario);
+        }
+
+        // POST: TipoUsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TipoUsuario tipoUsuario)
+        {
+            try
+            {
+                TipoUsuario? tipoUsuarioDto = await _context.TiposUsuario
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+                if (tipoUsuarioDto == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                tipoUsuarioDto.Descripcion = tipoUsuario.Descripcion;
+                tipoUsuarioDto.ModifyAt = DateTime.Now;
+                tipoUsuarioDto.ModifyBy = 0;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: TipoUsuarioController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            TipoUsuario? tipoUsuarioDto = await _context.TiposUsuario
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            return PartialView("_Delete", tipoUsuarioDto);
+        }
+
+        // POST: TipoUsuarioController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                TipoUsuario? tipoUsuarioDto = await _context.TiposUsuario
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+                if (tipoUsuarioDto == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                tipoUsuarioDto.Estatus = EstatusTipoUsuario.Inactivo;
+                tipoUsuarioDto.ModifyAt = DateTime.Now;
+                tipoUsuarioDto.ModifyBy = 0;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
---
+++ UHabitacional_Web/wwwroot/js/TipoUsuario.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function() {
+    $(".tipousuario-create").on("click", function() {
+        var id = $(this).data("id");
+        $("#tipoUsuarioCreate").load("/TipoUsuario/Create", function() {
+            $("#tipoUsuarioCreate").modal("show");
+        });
+    });
+
+    $(".tipousuario-edit").on("click", function() {
+        var id = $(this).data("id");
+        $("#tipoUsuarioEdit").load("/TipoUsuario/Edit/" + id, function() {
+            $("#tipoUsuarioEdit").modal("show");
+        });
+    });
+
+    $(".tipousuario-remove").on("click", function() {
+        var id = $(this).data("id");
+        $("#tipoUsuarioRemove").load("/TipoUsuario/Delete/" + id, function() {
+            $("#tipoUsuarioRemove").modal("show");
+        });
+    });
+});
