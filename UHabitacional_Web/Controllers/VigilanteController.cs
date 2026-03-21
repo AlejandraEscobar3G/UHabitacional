22546eea87b85b83ec7a6a08c4ba0ecea209ab40
Alejandra Escobar <ale.fesico@gmail.com>
2026-03-20 20:45:19 -0600

    Se agregan vistas y logica CRUD para vigilantes

---
+++ UHabitacional_Web/Controllers/VigilanteController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UHabitacional_Web.Models;
+
+namespace UHabitacional_Web.Controllers
+{
+    public class VigilanteController : Controller
+    {
+        private readonly UHabitacionalContext _context;
+        public VigilanteController(UHabitacionalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VigilanteController
+        public async Task<IActionResult> Index()
+        {
+            List<Usuario> usuarios = await _context.Usuarios
+                .Where(u => u.TipoUsuarioId == 3)
+                .ToListAsync();
+
+            return View(usuarios);
+        }
+
+        // GET: VigilanteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VigilanteController/Create
+        public async Task<IActionResult> Create()
+        {
+            return PartialView("_Create", new Usuario());
+        }
+
+        // POST: VigilanteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario usuario)
+        {
+            try
+            {
+                usuario.Estatus = EstatusUsuario.Activo;
+                usuario.TipoUsuarioId = 3;
+                usuario.CreatedAt = DateTime.Now;
+                usuario.CreatedBy = 0;
+
+                _context.Add(usuario);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: VigilanteController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            Usuario? usuario = await _context.Usuarios
+                    .FirstOrDefaultAsync(u => u.Id == id && u.TipoUsuarioId == 3);
+
+            return PartialView("_Edit", usuario);
+        }
+
+        // POST: VigilanteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Usuario usuario)
+        {
+            try
+            {
+                Usuario? usuarioDto = await _context.Usuarios
+                    .FirstOrDefaultAsync(u => u.Id == id && u.TipoUsuarioId == 3);
+
+                if (usuarioDto == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                usuarioDto.Nombre = usuario.Nombre;
+                usuarioDto.ApellidoPaterno = usuario.ApellidoPaterno;
+                usuarioDto.ApellidoMaterno = usuario.ApellidoMaterno;
+                usuarioDto.Correo = usuario.Correo;
+                usuarioDto.Estatus = usuario.Estatus;
+                usuarioDto.ModifyAt = DateTime.Now;
+                usuarioDto.ModifyBy = 0;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: VigilanteController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Usuario? usuarioDto = await _context.Usuarios
+                    .Where(u => u.Id == id && u.TipoUsuarioId == 3)
+                    .Include(u => u.TipoUsuario)
+                    .FirstOrDefaultAsync();
+
+            return PartialView("_Delete", usuarioDto);
+        }
+
+        // POST: VigilanteController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                Usuario? usuarioDto = await _context.Usuarios
+                    .Where(u => u.Id == id && u.TipoUsuarioId == 3)
+                    .Include(u => u.TipoUsuario)
+                    .FirstOrDefaultAsync();
+
+                if (usuarioDto == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                usuarioDto.Estatus = EstatusUsuario.Inactivo;
+                usuarioDto.ModifyAt = DateTime.Now;
+                usuarioDto.ModifyBy = 0;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
---
+++ UHabitacional_Web/wwwroot/js/Vigilante.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function() {
+    $(".vigilante-create").on("click", function() {
+        var id = $(this).data("id");
+        $("#vigilanteCreate").load("/Vigilante/Create", function() {
+            $("#vigilanteCreate").modal("show");
+        });
+    });
+
+    $(".vigilante-edit").on("click", function() {
+        var id = $(this).data("id");
+        $("#vigilanteEdit").load("/Vigilante/Edit/" + id, function() {
+            $("#vigilanteEdit").modal("show");
+        });
+    });
+
+    $(".vigilante-remove").on("click", function() {
+        var id = $(this).data("id");
+        $("#vigilanteRemove").load("/Vigilante/Delete/" + id, function() {
+            $("#vigilanteRemove").modal("show");
+        });
+    });
+});
